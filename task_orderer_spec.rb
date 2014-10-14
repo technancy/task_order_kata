f40c61083dfefedce613c652ae17819ba58a02ac
technancy <nancytechy@gmail.com>
2014-10-14 15:52:55 +0100

    write a test containing a dependancy

--- task_orderer_spec.rb
+++ task_orderer_spec.rb
@@ -2,10 +2,10 @@
 require_relative 'task_orderer'
 
 describe 'splitting job strings' do
-   
-    subject(:task_order) { TaskOrder.new(job) }
+  subject(:task_order) { TaskOrder.new(job) }
+
   context 'passing one job' do
-     let(:job) {'a'}
+    let(:job) {'a'}
     it 'handles a single job' do
         expect(task_order.sanitise).to eq(['a'])
     end
@@ -16,5 +16,12 @@ describe 'splitting job strings' do
     it 'handles two independant jobs' do
         expect(task_order.sanitise).to eq(['a','b'])
     end
+  end
+
+   context 'passing a single job containing a dependancy' do
+     let(:job) {'b <= a'}
+     it 'handles a dependant job' do
+        expect(task_order.sanitise).to eq(['b','a'])
+     end
   end
 end
